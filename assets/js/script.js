5c2113225f0f73963749a158720dd40f76a9d903
VHarriz <128908597+VHarriz@users.noreply.github.com>
2023-05-24 02:28:00 -0400

    start logic

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+
+
+var quizInfo = [{question: 'What is 4 x 2?', answer: '8'},{question: 'What is 1 - 1?', answer: '0'},{question: 'What is 50 / 2?', answer: '25'},{question: 'What is 7 + 7?', answer: '14'},{question: 'What is 50 + 50?', answer: '100'},{question: 'What is Fifteen plus Fifteen?', answer: 'Thirty'},{question: 'What is Zero minus Zero?', answer: 'Zero'},{question: 'What is 45 +45?', answer: '90'} ];
+// console.log(quizInfo);
+
+// const indexOfI = quizInfo.indexOf(question,[i]);
+console.log(Object.keys(quizInfo[3]));
+// console.log(indexOfI);
+// if (quizInfo.question === quizInfo.answer)
